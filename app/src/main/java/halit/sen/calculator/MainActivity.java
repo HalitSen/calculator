f021d4a5f8cb1f44fc9f5ec1cf45a6376ce40e9d
Halit Sen <sen.halitt@gmail.com>
2019-03-03 22:31:33 +0300

    add ripple effect feature

--- app/src/main/java/halit/sen/calculator/MainActivity.java
+++ app/src/main/java/halit/sen/calculator/MainActivity.java
@@ -2,12 +2,28 @@ package halit.sen.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.seven_tv)
+    TextView sevenText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.seven_tv)
+    void onSevenClicked(){
+        Log.i("7","seven clicked");
+    }
+
 }
